78b7a944c0df8f429dce14107d51b5db7844f164
xangmuve <cesarabel@johnmay.com.ni>
2015-12-23 20:21:11 -0600

    carga manual

--- metro/settings.py
+++ metro/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'django',
     'USER': 'dev',
-    'PASSWORD': 'multipagos',
+    'PASSWORD': 'postgres',
     'HOST': 'localhost',
     'PORT': '5432',
 }
