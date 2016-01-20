7049c7bb58f2532e52b810b11deebcfbaae8a2a7
xangmuve <cesarabel@johnmay.com.ni>
2016-01-19 20:32:29 -0600

    fix no firma

--- movil/views.py
+++ movil/views.py
@@ -224,7 +224,10 @@ def get_paquete(request):
             p.recibe = obj_json['Recibe']
             p.position = Geoposition(obj_json['Latitude'],
                 obj_json['Longitude'])
-            p.imagen = request.FILES['Imagen']
+            try:
+                p.imagen = request.FILES['Imagen']
+            except:
+                pass
             p.exportado = True
             obj_json['Mensaje'] = "Paquete cargado Correctamente"
             p.save()
