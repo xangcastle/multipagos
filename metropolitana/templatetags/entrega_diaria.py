6cf0ebd6d1cc56a61c825c9732eee7377c3d7d69
xangmuve <cesarabel@johnmay.com.ni>
2016-02-15 11:52:08 -0600

    new dashboard

--- metropolitana/templatetags/entrega_diaria.py
+++ metropolitana/templatetags/entrega_diaria.py
@@ -14,7 +14,7 @@ class entrega_diaria_Node(template.Node):
 
     def render(self, context):
             context[self.varname] = entrega_diaria.objects.all().order_by(
-                '-dia')[:100]
+                '-dia')[:200]
             return ''
 
 
