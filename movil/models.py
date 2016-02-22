4341075e01d021b3a12c9e03fcddfc38601181e1
xangmuve <cesarabel@johnmay.com.ni>
2016-02-22 13:44:08 -0600

    registrando perfil del usuario

--- movil/models.py
+++ movil/models.py
@@ -8,3 +8,7 @@ class UserProfile(models.Model):
     foto = models.ImageField(upload_to=get_media_url)
     zonas = models.ManyToManyField(Zona)
     celular = models.CharField(max_length=14, null=True)
+
+    class Meta:
+        verbose_name = 'usuario'
+        verbose_name_plural = "usuarios de entrega"
