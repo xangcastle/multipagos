be7fdbabe20b277f72664f255ca3235a5ed0765d
xangmuve <cesarabel@johnmay.com.ni>
2016-03-01 12:57:11 -0600

    fix error on integrar cliente

--- movil/views.py
+++ movil/views.py
@@ -250,6 +250,7 @@ def get_cartera(request):
         obj_json['departamento'] = c.departamento.name
         obj_json['municipio'] = c.municipio.name
         obj_json['barrio'] = c.barrio.name
+        obj_json['direccion'] = c.direccion
         obj_json['telefonos'] = c.telefonos
         obj_json['comentario'] = c.comentario
         facs = []
