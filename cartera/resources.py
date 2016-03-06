ced78dc47586ee97be75d096c0a0da956f5840c2
xangmuve <cesarabel@johnmay.com.ni>
2016-03-06 13:48:43 -0600

    formato subida de cartera corriente

--- cartera/resources.py
+++ cartera/resources.py
@@ -6,9 +6,9 @@ from .models import *
 class detalle_resouce(resources.ModelResource):
     class Meta:
         model = Detalle
-        fields = ('cliente', 'producto', 'categoria', 'contrato', 'nit',
-            'departamento', 'localidad', 'barr_contacto', 'cuenta_cobro',
-            'servicio', 'factura', 'factura_interna', 'no_cupon', 'no_fiscal',
+        fields = ('id', 'cliente', 'producto', 'categoria', 'contrato', 'nit',
+            'departamento', 'localidad', 'barr_contacto',
+            'servicio', 'factura_interna', 'factura', 'no_cupon', 'no_fiscal',
             'ciclo', 'ano', 'mes', 'fecha_fact', 'fecha_venc', 'tipo_mora',
             'estado_corte', 'fecha_instalacion', 'descr_plan', 'tecnologia',
             'canal_venta', 'ejecutivo_venta', 'facturas_generadas',
