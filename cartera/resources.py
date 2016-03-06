1f6ab60447aea3c23498b2a3d4552c68a09d85d1
xangmuve <cesarabel@johnmay.com.ni>
2016-03-06 16:48:38 -0600

    asignacion de tareas on verificaciones

--- cartera/resources.py
+++ cartera/resources.py
@@ -12,6 +12,11 @@ class ccorriente_resouce(resources.ModelResource):
             'saldo_pend_factura', 'ciclo', 'mes', 'ano', 'suscriptor',
             'direccion', 'tipo_cartera', 'comentario')
 
+        export_order = ('id', 'contrato', 'departamento', 'localidad',
+            'barr_contacto', 'servicio', 'factura', 'no_cupon',
+            'saldo_pend_factura', 'ciclo', 'mes', 'ano', 'suscriptor',
+            'direccion', 'tipo_cartera', 'comentario')
+
 
 class cmora_resouce(resources.ModelResource):
     class Meta:
@@ -26,6 +31,15 @@ class cmora_resouce(resources.ModelResource):
             'tel_contacto', 'tel_instalacion', 'tel_contacto_cliente',
             'suscriptor', 'direccion', 'fecha_asignacion', 'comentario')
 
+        export_order = ('id', 'cliente', 'producto', 'categoria', 'contrato',
+            'nit', 'departamento', 'localidad', 'barr_contacto',
+            'servicio', 'factura', 'no_fiscal',
+            'ciclo', 'ano', 'mes', 'fecha_fact', 'fecha_venc', 'tipo_mora',
+            'estado_corte', 'fecha_instalacion', 'descr_plan', 'tecnologia',
+            'canal_venta', 'ejecutivo_venta',
+            'tel_contacto', 'tel_instalacion', 'tel_contacto_cliente',
+            'suscriptor', 'direccion', 'fecha_asignacion', 'comentario')
+
 
 class crebaja_resouce(resources.ModelResource):
     class Meta:
@@ -33,6 +47,15 @@ class crebaja_resouce(resources.ModelResource):
 
         fields = ('id', 'cliente', 'producto', 'categoria', 'contrato', 'nit',
             'departamento', 'localidad', 'barr_contacto',
+            'servicio', 'factura', 'no_fiscal',
+            'ciclo', 'ano', 'mes', 'fecha_fact', 'fecha_venc', 'tipo_mora',
+            'estado_corte', 'fecha_instalacion', 'descr_plan', 'tecnologia',
+            'canal_venta', 'ejecutivo_venta',
+            'tel_contacto', 'tel_instalacion', 'tel_contacto_cliente',
+            'suscriptor', 'direccion', 'fecha_asignacion', 'comentario')
+
+        export_order = ('id', 'cliente', 'producto', 'categoria', 'contrato',
+            'nit', 'departamento', 'localidad', 'barr_contacto',
             'servicio', 'factura', 'no_fiscal',
             'ciclo', 'ano', 'mes', 'fecha_fact', 'fecha_venc', 'tipo_mora',
             'estado_corte', 'fecha_instalacion', 'descr_plan', 'tecnologia',
