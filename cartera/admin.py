1e443655e45e3a81ead852c7b7b8464030b21d79
xangmuve <cesarabel@johnmay.com.ni>
2016-03-10 09:35:15 -0600

    filtros y campos

--- cartera/admin.py
+++ cartera/admin.py
@@ -67,8 +67,10 @@ class entregas_cliente(base_tabular):
 
 class cliente_admin(admin.ModelAdmin):
     list_display = ('code', 'name', 'identificacion', 'saldo_total',
-        'comentario', 'tipo_mora', 'position_ver')
-    list_filter = ('departamento', 'municipio', 'position_ver', 'tipo_mora')
+        'comentario', 'tipo_mora', 'position_ver', 'departamento', 'municipio',
+        'barrio')
+    list_filter = ('departamento', 'municipio', 'barrio', 'position_ver',
+        'tipo_mora')
     search_fields = ('code', 'name', 'identificacion', 'contrato')
     readonly_fields = ('code', 'name', 'identificacion', 'departamento',
         'municipio', 'barrio', 'zona', 'comentario', 'direccion', 'tipo_mora')
