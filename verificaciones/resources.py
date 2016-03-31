cf94cb2cd38b3325338bca39d2bcfea5554ebd00
xangmuve <cesarabel@johnmay.com.ni>
2016-03-31 11:14:04 -0600

    fix order

--- verificaciones/resources.py
+++ verificaciones/resources.py
@@ -7,6 +7,7 @@ class verificacion_resouce(resources.ModelResource):
     class Meta:
         model = Verificacion
         fields = ('id', 'fecha_alta', 'nombre_cliente', 'contrato', 'plan',
-            'cedula', 'servicio', 'categoria', 'sucursal', 'departamento',
-            'municipio', 'barrio', 'direccion', 'telefono', 'celular',
-            'costo_instalacion', 'equipo', 'serial', 'mac', 'sim', 'solicitud')
+            'cedula', 'servicio', 'categoria', 'sucursal', 'vendedor',
+            'departamento', 'municipio', 'barrio', 'direccion', 'telefono',
+            'celular', 'costo_instalacion', 'equipo', 'serial', 'mac', 'sim',
+            'solicitud')
