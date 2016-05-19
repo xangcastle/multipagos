31c62fce10a7ec4b44afccf7fed22916a66dded7
xangmuve <cesarabel@johnmay.com.ni>
2016-05-19 00:53:45 -0600

    panel de asignaciones

--- home/views.py
+++ home/views.py
@@ -14,6 +14,24 @@ class barrios_huerfanos(TemplateView):
     template_name = "home/barrios_huerfanos.html"
 
 
+class reporte_gestiones(TemplateView):
+    template_name = "home/gestiones.html"
+
+
+class panel_asignacion(TemplateView):
+    template_name = "home/asignaciones.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(panel_asignacion, self).get_context_data(**kwargs)
+        context['zonas'] = Zona.objects.all().order_by('name')
+        context['mensaje'] = \
+        'En esta seccion usted podra asignar las distintas tareas'
+        context['mensaje'] += \
+        ' en cada barrio al gestor hasta una cantidad maxima de x entregas'
+        context['msgclass'] = 'info'
+        return context
+
+
 @csrf_exempt
 def info_barrio(request):
     if request.is_ajax:
