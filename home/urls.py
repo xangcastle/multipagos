de249dcfb10981b22ee4a20aaa88a6b6ea2cc308
xangmuve <cesarabel@johnmay.com.ni>
2016-05-19 01:15:53 -0600

    login

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,19 @@
+
 from django.conf.urls import *
 from .views import *
+from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = patterns('home.views',
     url(r'^info_barrio/$', 'info_barrio', name='info_barrio'),
-    url(r'^barrios_huerfanos/$', barrios_huerfanos.as_view(),
+    url(r'^barrios_huerfanos/$',
+        login_required(barrios_huerfanos.as_view(), login_url='/admin/login/'),
         name='barrios_huerfanos'),
-    url(r'^reporte_gestiones/$', reporte_gestiones.as_view(),
+    url(r'^reporte_gestiones/$',
+        login_required(reporte_gestiones.as_view(), login_url='/admin/login/'),
         name='reporte_gestiones'),
-    url(r'^panel_asignacion/$', panel_asignacion.as_view(),
+    url(r'^panel_asignacion/$',
+        login_required(panel_asignacion.as_view(), login_url='/admin/login/'),
         name='panel_asignacion'),
     url(r'^asignar_barrio/$', 'asignar_barrio', name='asignar_barrio'),
 )
